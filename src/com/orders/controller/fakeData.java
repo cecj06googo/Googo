252c99610022d7b257812aee747a8477a9a5903c
masonRuan <ggyy40706@yahoo.com.tw>
2014-11-01 00:03:37 +0800

    Revert "Revert "Revert "Revert "Revert "Git Extensions 新工具試用 + 訂單功能整合"""""
    This reverts commit 6b64d88ef70a7978c2563f503576abd485e1c677.

---
+++ src/com/orders/controller/fakeData.java
@@ -0,0 +1,69 @@
+package com.orders.controller;
+
+import java.io.IOException;
+
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+
+public class fakeData extends HttpServlet{
+	private static final long serialVersionUID = 1L;
+	public void doGet(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
+		doPost(request, response);
+	}
+
+	protected void doPost(HttpServletRequest request,
+			HttpServletResponse response) throws ServletException, IOException {
+		request.setCharacterEncoding("UTF-8");
+		
+		String action = request.getParameter("action");
+		HttpSession session = request.getSession();
+		
+		if ("insert".equals(action)) {
+		
+		String com_id = request.getParameter("com_id");
+		String mem_id = request.getParameter("mem_id");
+		
+		session.setAttribute("com_id", com_id);  //session存值
+		session.setAttribute("mem_id", mem_id);  //session存值
+		
+		/******************** (Send the Success view) ************/
+		String url = "/productsDetails.jsp";
+		RequestDispatcher successView = request
+				.getRequestDispatcher(url);									
+		successView.forward(request, response);
+		}// end insert
+		
+		if ("select".equals(action)) {
+			String user_id = request.getParameter("user_id");
+			//登入時要去資料庫檢查看帳號是屬於com_id(商家)還是mem_id(會員)
+			session.setAttribute("user_id", user_id);  //session存值
+			
+			/******************** (Send the Success view) ************/
+			String url = "/ordersSearch.jsp";
+			RequestDispatcher successView = request
+					.getRequestDispatcher(url);									
+			successView.forward(request, response);
+		}// end select
+		
+		
+		if ("login".equals(action)) {
+			String user_id = request.getParameter("user_id");
+			//登入時要去資料庫檢查看帳號是屬於com_id(商家)還是mem_id(會員)
+			session.setAttribute("user_id", user_id);  //session存值
+			
+			/******************** (Send the Success view) ************/
+			String url = "/orderOnLoad.gg";
+			
+			RequestDispatcher successView = request
+					.getRequestDispatcher(url);									
+			successView.forward(request, response);
+		}// end login
+		
+	}
+
+}
