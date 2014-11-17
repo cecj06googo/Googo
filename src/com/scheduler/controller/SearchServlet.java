5411bb82d9d820ba1e13aca8820c8b71814c5003
e810616 <e810616@yahoo.com.tw>
2014-11-17 13:30:41 +0800

    初步

---
+++ src/com/scheduler/controller/SearchServlet.java
@@ -0,0 +1,26 @@
+package com.scheduler.controller;
+
+import java.io.IOException;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+public class SearchServlet extends HttpServlet {
+
+	private static final long serialVersionUID = 1L;
+	public void doGet(HttpServletRequest req, HttpServletResponse res)
+			throws ServletException, IOException {
+		doPost(req, res);
+	}
+
+	public void doPost(HttpServletRequest req, HttpServletResponse res)
+			throws ServletException, IOException {
+
+		req.setCharacterEncoding("UTF-8");
+		
+}
+
+
+}
