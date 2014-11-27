f4001f75a3b4e0dcf2824a90330747ee8342b87f
antai0926 <antai0926@gmail.com>
2014-11-28 01:40:12 +0800

    宜蘭旅遊推薦商家(寫定)OK

--- src/com/search/controller/IndexPlaceRecommend.java
+++ src/com/search/controller/IndexPlaceRecommend.java
@@ -15,22 +15,31 @@ import com.company.model.*;
 public class IndexPlaceRecommend extends HttpServlet{
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		//int comID=0;
+		//System.out.println("我有進來IndexPlaceRecommend.java doGet");
 		List<CompanyVO> comvos = new ArrayList<CompanyVO>();
 		int [] comids = {19,20,21,22};
-		for(int comID : comids){
-			CompanyDAO comdao = new CompanyDAO();
-			CompanyVO comvo = comdao.findByPrimaryKey(comID);
-			comvos.add(comvo);
+		try{
+			for(int comID : comids){
+				CompanyDAO comdao = new CompanyDAO();
+				CompanyVO comvo = comdao.findByPrimaryKey(comID);
+				comvos.add(comvo);
+				System.out.println("comName="+comvo.getComName());
+			}
+		}catch(Exception e){
+			//System.out.println("IndexPlaceRecommend的DAO處裡錯誤");
+			e.printStackTrace();
 		}
 		request.setAttribute("comvos", comvos);
 
 		RequestDispatcher rd = request.getRequestDispatcher("/_03_research/site.jsp");
-		//System.out.println("SearchServlete執行完畢轉跳research.jsp");
+		//System.out.println("IndexPlaceRecommend執行完畢轉跳site.jsp");
 		rd.forward(request, response);
 		
 		
 	}
 	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		//System.out.println("我有進來IndexPlaceRecommend.java doPost");
 		doPost(request,response);
 	}
+	
 }
