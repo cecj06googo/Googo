165e686d25c698937ded3ae3b36524c955e4ef56
raniantheone <raniantheone@gmail.com>
2014-12-02 12:49:59 +0800

    搬移測試OK，修正了配備動態元件的重複顯示bug

--- WebContent/js/form_builder.js
+++ WebContent/js/form_builder.js
@@ -351,13 +351,15 @@ $(function() {
                     select = el.find('select'),
                     label = el.find('label');
                     //list_options = "";
-
+                
+                $(select).empty();
                 $(pre_databean).each(function(index){
                 	var option = $("<option></option>");
                 	option.val(pre_databean[index].price);
                 	option.text(pre_databean[index].description);
                 	select.append(option);
                 });
+                $("<option>請下拉選擇配備</option>").val(0).prependTo(select);
 
                 label.text($(this.prefix + 'label').val()).attr('for', select.attr('id'));
                 
