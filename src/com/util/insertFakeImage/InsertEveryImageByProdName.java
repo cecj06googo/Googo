42aeede6cf0bcef63fa4e465567d9e5c429125ea
antai0926 <antai0926@gmail.com>
2014-12-03 04:35:29 +0800

    補圖片

---
+++ src/com/util/insertFakeImage/InsertEveryImageByProdName.java
@@ -0,0 +1,36 @@
+package com.util.insertFakeImage;
+
+import java.sql.Connection;
+
+import com.util.ConnectionUtil;
+
+public class InsertEveryImageByProdName {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Connection conn;
+		ConnectionUtil conutil = new ConnectionUtil();
+		conn = conutil.getConnection();
+		InsertFakeImageDAO insertFakeImg = new InsertFakeImageDAO(conn);
+		String prodName="賓士E-320";
+		String srcMainProdPic ="WebContent/img/demoCars/BENZ_M320_01.jpg";
+		String srcSub1Pic = "WebContent/img/demoCars/BENZ_M320_02.jpg";
+		String srcSub2Pic = "WebContent/img/demoCars/BENZ_M320_03.jpg";
+		String srcSub3Pic = "WebContent/img/demoCars/BENZ_M320_04.jpg";
+		InsertFakeImageDAO c2 = new InsertFakeImageDAO(conn);
+		
+		try{
+			insertFakeImg.InsertImagesByProdName(prodName, srcMainProdPic, srcSub1Pic, srcSub2Pic, srcSub3Pic);
+			insertFakeImg.InsertImagesByProdName("法拉利458", "WebContent/img/demoCars/FERRARI_458_01.jpg", "WebContent/img/demoCars/FERRARI_458_02.jpg", "WebContent/img/demoCars/FERRARI_458_03.jpg", "WebContent/img/demoCars/FERRARI_458_04.jpg");
+			insertFakeImg.InsertImagesByProdName("Mini-Cooper-2014", "WebContent/img/demoCars/MINI_COOPER_01.jpg", "WebContent/img/demoCars/MINI_COOPER_02.jpg", "WebContent/img/demoCars/MINI_COOPER_03.jpg", "WebContent/img/demoCars/MINI_COOPER_04.jpg");
+			insertFakeImg.InsertImagesByProdName("野狼125", "WebContent/img/demoCars/125-1.jpg", "WebContent/img/demoCars/125-2.jpg", "WebContent/img/demoCars/125-3.jpg", "WebContent/img/demoCars/125-4.jpg");
+			insertFakeImg.InsertImagesByProdName("捷安特GreatJourney", "WebContent/img/demoCars/GJ1-1.jpg", "WebContent/img/demoCars/GJ1-2.jpg", "WebContent/img/demoCars/GJ1-3.jpg", "WebContent/img/demoCars/GJ1-4.jpg");
+			
+		}catch(Exception e){
+			
+		}finally {
+			ConnectionUtil.closeConnection(conn);
+		}
+	}
+
+}
