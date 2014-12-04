e429e01c68621af762357126231d428c47470332
Antai Li <antai0926@gmail.com>
2014-12-04 18:27:00 +0800

    新增三輛車圖片的sql及java

--- src/com/util/insertFakeImage/InsertEveryImageByProdName.java
+++ src/com/util/insertFakeImage/InsertEveryImageByProdName.java
@@ -12,17 +12,17 @@ public class InsertEveryImageByProdName {
 		ConnectionUtil conutil = new ConnectionUtil();
 		conn = conutil.getConnection();
 		InsertFakeImageDAO insertFakeImg = new InsertFakeImageDAO(conn);
-		String prodName="Benz-E-320";
-		String srcMainProdPic ="WebContent/img/demoCars/BENZ_M320_01.jpg";
-		String srcSub1Pic = "WebContent/img/demoCars/BENZ_M320_02.jpg";
-		String srcSub2Pic = "WebContent/img/demoCars/BENZ_M320_03.jpg";
-		String srcSub3Pic = "WebContent/img/demoCars/BENZ_M320_04.jpg";
-		
-		try{
-			insertFakeImg.InsertImagesByProdName(prodName, srcMainProdPic, srcSub1Pic, srcSub2Pic, srcSub3Pic);
+	
+		try{//-------------汽車區域------------------------------
+			insertFakeImg.InsertImagesByProdName("Benz-E-320","WebContent/img/demoCars/BENZ_M320_01.jpg","WebContent/img/demoCars/BENZ_M320_02.jpg","WebContent/img/demoCars/BENZ_M320_03.jpg","WebContent/img/demoCars/BENZ_M320_04.jpg");
 			insertFakeImg.InsertImagesByProdName("Ferrari-458", "WebContent/img/demoCars/FERRARI_458_01.jpg", "WebContent/img/demoCars/FERRARI_458_02.jpg", "WebContent/img/demoCars/FERRARI_458_03.jpg", "WebContent/img/demoCars/FERRARI_458_04.jpg");
 			insertFakeImg.InsertImagesByProdName("Mini-Cooper-2014", "WebContent/img/demoCars/MINI_COOPER_01.jpg", "WebContent/img/demoCars/MINI_COOPER_02.jpg", "WebContent/img/demoCars/MINI_COOPER_03.jpg", "WebContent/img/demoCars/MINI_COOPER_04.jpg");
+			insertFakeImg.InsertImagesByProdName("馬自達-3","WebContent/img/demoCars/m3_01.jpg","WebContent/img/demoCars/m3_02.jpg","WebContent/img/demoCars/m3_03.jpg","WebContent/img/demoCars/m3_04.jpg");
+			insertFakeImg.InsertImagesByProdName("豐田-Yaris","WebContent/img/demoCars/Toyota_01.jpg","WebContent/img/demoCars/Toyota_02.jpg","WebContent/img/demoCars/Toyota_03.jpg","WebContent/img/demoCars/Toyota_04.jpg");
+			insertFakeImg.InsertImagesByProdName("保時捷-911","WebContent/img/demoCars/porsche_01.jpg","WebContent/img/demoCars/porsche_02.jpg","WebContent/img/demoCars/porsche_03.jpg","WebContent/img/demoCars/porsche_04.jpg");
+			//-------------機車區域------------------------------
 			insertFakeImg.InsertImagesByProdName("野狼125", "WebContent/img/demoCars/125-1.jpg", "WebContent/img/demoCars/125-2.jpg", "WebContent/img/demoCars/125-3.jpg", "WebContent/img/demoCars/125-4.jpg");
+			//-------------腳踏車區域------------------------------
 			insertFakeImg.InsertImagesByProdName("捷安特GreatJourney", "WebContent/img/demoCars/GJ1-1.jpg", "WebContent/img/demoCars/GJ1-2.jpg", "WebContent/img/demoCars/GJ1-3.jpg", "WebContent/img/demoCars/GJ1-4.jpg");
 			insertFakeImg.InsertImagesByProdName("MERIDA-FB-106", "WebContent/img/demoCars/m-1.jpg", "WebContent/img/demoCars/m-2.jpg", "WebContent/img/demoCars/m-3.jpg", "WebContent/img/demoCars/m-4.jpg");
 			insertFakeImg.InsertImagesByProdName("Audi電動腳踏車", "WebContent/img/demoCars/A-1.jpg", "WebContent/img/demoCars/A-2.jpg", "WebContent/img/demoCars/A-3.jpg", "WebContent/img/demoCars/A-4.jpg");
