2d37b31e615bd36b399818317e0fd32171b82b94
An-Tai Li <antai0926@gmail.com>
2015-10-31 22:48:55 +0800

    修改連線資訊

--- src/com/util/ConnectConstant.java
+++ src/com/util/ConnectConstant.java
@@ -2,7 +2,7 @@ package com.util;
 
 public class ConnectConstant {
 	public final static String driver = "com.microsoft.sqlserver.jdbc.SQLServerDriver";
-	public final static String url = "jdbc:sqlserver://i64nhzkqmt.database.windows.net:1433;database=GGDB";
-	public final static String userid = "googocar@i64nhzkqmt";
-	public final static String passwd = "Sa123456";
+	public final static String url = "jdbc:sqlserver://hlt6wi28n9.database.windows.net:1433;database=GGDB";
+	public final static String userid = "Googouser@hlt6wi28n9";
+	public final static String passwd = "Googo123!";
 }
